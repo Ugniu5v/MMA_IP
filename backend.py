96b9f9bbb36d492a2fb4dccfd8643dc27c479083
Ugniu5v <ugniusvarzukas@gmail.com>
2025-12-11 11:20:50 +0200

    backend scaled and encoded addition

--- backend.py
+++ backend.py
@@ -8,6 +8,8 @@ import pandas as pd
 app = Flask(__name__, static_folder=".", static_url_path="")
 CORS(app)
 model = None
+scalers = {}
+encoders = {}
 
 
 def train_model():
@@ -40,6 +42,25 @@ def load_model():
         with open("model.pkl", "rb") as f:
             model = pickle.load(f)
 
+def load_encoders():
+    global encoders
+    try:
+        if os.path.exists("encoders.pkl"):
+            with open("encoders.pkl", "rb") as f:
+                encoders = pickle.load(f)
+    except FileNotFoundError:
+        print("encoders.pkl is missing")
+
+
+def load_scalers():
+    global scalers
+    try:
+        if os.path.exists("scalers.pkl"):
+            with open("scalers.pkl", "rb") as f:
+                scalers = pickle.load(f)
+    except FileNotFoundError:
+        print("scalers.pkl is missing")
+
 
 @app.route("/")
 def index():
@@ -50,6 +71,8 @@ def index():
 @app.route("/predict", methods=["POST"])
 def predict():
     load_model()
+    load_encoders()
+    load_scalers()
     # Empty field value: None
 
     # Not encoded data (strings)
@@ -73,14 +96,35 @@ def predict():
         )
     )
 
-    return str(result)
+    formatted_result = f"{result:,.2f}"
+    return str(formatted_result)
 
 
 def do_something(x):
     global model
+    global scalers
+    global encoders
+
+    # Encode categorical values
+    for col, encoder in encoders.items():
+        if col in x.columns:
+            x[col] = encoder.transform(x[[col]])
+
+    # Scale continues inputs
+    for col, scaler in scalers.items():
+        if col in x.columns:
+            x[col] = scaler.transform(x[[col]]).ravel()
 
     prediction = model.predict(x)
-    return prediction[0]
+
+    # Convert scaled prediction back to original price
+    if "price" in scalers:
+        prediction = scalers["price"].inverse_transform(
+            prediction.reshape(-1, 1)
+        )
+
+    return float(prediction.ravel()[0])
+
 
 
 if __name__ == "__main__":
